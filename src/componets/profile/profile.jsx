09417cf57c9a92cd565a68c3af96684285e5a6a7
gabssenai <gabsalcaide@gmail.com>
2023-08-10 17:11:45 -0300

    Finalizando aula - 10/08

---
+++ src/componets/profile/profile.jsx
@@ -0,0 +1,8 @@
+export default function profile() {
+    return (
+        <div className="profile">
+            <img src="" alt=""></img>
+            <p>@GabsAlcaide</p>
+        </div>
+    )
+}
---
+++ src/pages/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Profile from "../componets/profile/profile";
+
+export default function App() {
+    return (
+        <>
+            <Profile></Profile>
+        </>
+    );
+};
