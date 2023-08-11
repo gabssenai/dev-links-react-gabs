50d35727b58d83b2616be3514e084b9544d60e70
gabssenai <gabsalcaide@gmail.com>
2023-08-11 10:20:31 -0300

    desenvolvido o profile

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,21 @@
-import "./App.css";
+import { useState } from "react";
 import Profile from "../componets/profile/profile";
 
-export default function App() {
+import "./App.css";
+
+import imgDarkMode from "../img/amongescuro.png";
+import imgLightMode from "../img/amongclaro.webp";
+
+const App = () => {
+    const [isLightMode, setIsLightMode] = useState(false);
+
+    const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
+    const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no Modo Escuro"
     return (
-        <>
-            <Profile></Profile>
-        </>
-    );
-};
+        <div>
+            <Profile imgSrc={imgSrc} imgAlt={imgAlt}>@Gabs</Profile>
+        </div>
+    )
+}
+
+export default App;
